90e24958821884e562c3464984e294c294225ebe
zsniper <zkman88@gmail.com>
2015-03-21 13:44:11 -0400

    final values in Round 3

--- event_loop.py
+++ event_loop.py
@@ -14,6 +14,7 @@ def event_loop():
     global secs
     try: 
         secs = clientpy2.securities()
+        print 'hi'
         #print secs['HOG']['net_worth']
         tickers = secs.keys()
 
@@ -42,9 +43,9 @@ def event_loop():
                 max_ticker_value = new_value
                 max_ticker_name = ticker
 
-        print 'ticker_values1: ' + str(ticker_values)
+        # print 'ticker_values1: ' + str(ticker_values)
         ticker_values = sorted(ticker_values, key=value_compare)[::-1]
-        print 'ticker_values2: ' + str(ticker_values)
+        # print 'ticker_values2: ' + str(ticker_values)
 
         current_cash = float(clientpy2.my_cash())*0.75
         while True:
@@ -52,13 +53,13 @@ def event_loop():
             min_sell_value = min_sell_value + 0.05
             shares_bought = int(current_cash//min_sell_value)
             clientpy2.bid(max_ticker_name, min_sell_value, shares_bought)
-            print 'bid: max_ticker_name: ' + str(max_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
+            # print 'bid: max_ticker_name: ' + str(max_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
             a = clientpy2.my_securities(max_ticker_name, 'shares')
             if a > 0:
-                print 'order bid: max_ticker_name: ' + str(max_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
+                # print 'order bid: max_ticker_name: ' + str(max_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
                 break
 
-        threshold = 0.00075
+        threshold = 0.0003
 
         second_ticker_name = ''
         third_ticker_name = ''
@@ -69,23 +70,24 @@ def event_loop():
             max_ticker_name = ticker_values[i]['ticker']
             while clientpy2.my_securities(max_ticker_name, 'dividend_ratio') > threshold:
                 second_ticker_name = ticker_values[(i+1)%len(tickers)]['ticker']
-                min_sell_value = float(clientpy2.min_sell(second_ticker_name))
+                min_sell_value = float(clientpy2.min_sell(second_ticker_name)) + 0.0001
 
                 current_cash = float(clientpy2.my_cash())
                 shares_bought = int(current_cash//min_sell_value)
                 
                 clientpy2.bid(second_ticker_name, min_sell_value, shares_bought)
-                print 'bid: second_ticker_name: ' + str(second_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
+                # print 'bid: second_ticker_name: ' + str(second_ticker_name) + ' min_sell_value: ' + str(min_sell_value) + ' shares_bought: ' + str(shares_bought)
             
             sale_price = float(clientpy2.min_sell(max_ticker_name))
             we_have_left = int(clientpy2.my_securities(max_ticker_name, 'shares'))
             while we_have_left > 0:
                 clientpy2.ask(max_ticker_name, sale_price, we_have_left)
-                print 'ask: max_ticker_name: ' + str(max_ticker_name) + ' sale_price: ' + str(sale_price) + ' we_have_left: ' + str(we_have_left)
+                # print 'ask: max_ticker_name: ' + str(max_ticker_name) + ' sale_price: ' + str(sale_price) + ' we_have_left: ' + str(we_have_left)
                 we_have_left = int(clientpy2.my_securities(max_ticker_name, 'shares'))
                 sale_price = sale_price - 0.01
 
             i = (i+1) % len(tickers)
+
     finally:
         clientpy2.closeSocket()
 
