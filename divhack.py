936aebec9bb117a5cf3a696ab7383a108ed09745
ilia10000 <s.ilia@rogers.com>
2015-03-21 03:52:38 -0400

    Update divhack.py

--- divhack.py
+++ divhack.py
@@ -1,7 +1,12 @@
 import clientpy2
-
+bought_at = {}
+    tickers = clientpy2.securities().keys()
+    for ticker in tickers:
+        bought_at[ticker]=0
 def on_divhack_event(e):
     our_orders = 0 #get_our_orders(e['ticker'], e['price'])
+    
+    
     if e['action'] == 'ASK':
         price = get_worth_price(e['ticker'], e['price'])
         # if is_in_our_orders(e['ticker'], our_orders):
@@ -13,6 +18,7 @@ def on_divhack_event(e):
         if price > 0:
             shares_to_sell = get_shares_to_sell(our_orders, e['ticker'], e['price'])
             clientpy2.bid(e['ticker'], price, shares_to_sell)
+            bought_at[e['ticker']]=price
             new_sell_price = get_new_sell_price(our_orders, e['ticker'], e['price'])
             clientpy2.ask(e['ticker'], new_sell_price, shares_to_sell)
             print 'bidding: ' + str(e['ticker']) + ' price: ' + str(price) + ' shares_to_sell: ' + str(shares_to_sell) + '\n'
@@ -23,7 +29,7 @@ def on_divhack_event(e):
     return False
 
 def get_worth_price(ticker, price):
-    return price + 0.1
+    return price + 0.05
 
 def get_undercut_price(ticker, price):
     return price + 0.1
@@ -33,15 +39,6 @@ def get_shares_to_sell(our_orders, ticker, price):
     # return our_orders['shares']
 
 def get_new_sell_price(our_orders, ticker, price):
-    return price - 0.01
-
-def is_in_our_orders(ticker, our_orders):
-    print 'is_in_our_orders' + str(ticker) + '\n'
-    for action in our_orders:
-        if action['action'] == 'ASK':
-            return True
-    return False
-
-def get_our_orders(ticker, price):
-    print 'get_our_orders' + str(ticker) + '\n'
-    return clientpy2.my_orders(ticker)
+    modifier= 0.15
+    rate = rates.get(ticker)
+    return bought_at[ticker]*(1 + rate - modifier)
