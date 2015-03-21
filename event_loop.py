bcca7742e20f59017e7f14985b1a08e8b629ea25
zsniper <zkman88@gmail.com>
2015-03-21 07:45:09 -0400

    added liquidation sorta

--- event_loop.py
+++ event_loop.py
@@ -13,6 +13,8 @@ def event_loop():
     rotation = 0
     activetickers = []
 
+    liquidate = 1
+
     while True:
         if rotation % 2 == 0:
             activetickers = tickers[0:numberoftickers/2]
@@ -21,24 +23,28 @@ def event_loop():
         order_npast = push_event(past_orders, activetickers)
 
         for x in order_npast['process']:
+            x['liquidate'] = liquidate
             events.on_event(x)
 
         past_orders = order_npast['hist']
         print 'tick'
-        time.sleep(0.1)
+        # time.sleep(0.1)
         rotation = (rotation + 1) % 2
+        liquidate = liquidate + 1
     #sleep thread
 
 #returns list to process and past orders.
 def push_event(past, tickers):
     process = []
     new_past = {}
-    for t in tickers:
-        o = clientpy2.orders(t) #list of  dictionaries
-        new_past[t] = o
-        for i in o:
-            if i not in past[t]:
-                i['ticker'] = t
-                process.append(i)
+    for ticker in tickers:
+        orders = clientpy2.orders(ticker) #list of  dictionaries
+        #rate = clientpy2.my_securities(ticker, 'dividend_ratio')
+        new_past[ticker] = orders
+        for order in orders:
+            if ticker not in past.keys() or order not in past[ticker]:
+                order['ticker'] = ticker
+        #        order['rate'] = rate
+                process.append(order)
     return {'process':process, 'hist':new_past}
 
--- events.py
+++ events.py
@@ -2,7 +2,10 @@ import clientpy2
 
 def on_event(e):
     our_orders = 0 #get_our_orders(e['ticker'], e['price'])
-    if e['action'] == 'ASK':
+    if e['liquidate'] % 5 == 0:
+        clientpy2.ask(e['ticker'], e['price']-0.05,5)
+        print 'asking: ' + str(e['ticker']) + ' price: ' + str(e['price']) + ' shares_to_sell: ' + str(5) + '\n'
+    elif e['action'] == 'ASK':
         price = get_worth_price(e['ticker'], e['price'])
         # if is_in_our_orders(e['ticker'], our_orders):
         #     undercut_price = get_undercut_price(e['ticker'], e['price'])
@@ -13,7 +16,7 @@ def on_event(e):
         if price > 0:
             shares_to_sell = get_shares_to_sell(our_orders, e['ticker'], e['price'])
             clientpy2.bid(e['ticker'], price, shares_to_sell)
-            new_sell_price = get_new_sell_price(our_orders, e['ticker'], e['price'])
+            new_sell_price = get_new_sell_price(our_orders, e['ticker'], e['price']) #, e['rate'])
             clientpy2.ask(e['ticker'], new_sell_price, shares_to_sell)
             print 'bidding: ' + str(e['ticker']) + ' price: ' + str(price) + ' shares_to_sell: ' + str(shares_to_sell) + '\n'
             print 'asking: ' + str(e['ticker']) + ' price: ' + str(new_sell_price) + ' shares_to_sell: ' + str(shares_to_sell) + '\n'
@@ -23,17 +26,14 @@ def on_event(e):
     return False
 
 def get_worth_price(ticker, price):
-    return price + 0.05
-
-def get_undercut_price(ticker, price):
-    return price + 0.1
+    return price + 0.02
 
 def get_shares_to_sell(our_orders, ticker, price):
-    return 1
+    return 5
     # return our_orders['shares']
 
-def get_new_sell_price(our_orders, ticker, price):
-    return price #- 0.01
+def get_new_sell_price(our_orders, ticker, price): #, rate):
+    return price + 0.02 #(1 + rate) # + 0.15) # + 0.05
 
 def is_in_our_orders(ticker, our_orders):
     print 'is_in_our_orders' + str(ticker) + '\n'
