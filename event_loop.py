3b70422bf1e220c1f0a7651ee603628b00559d0a
Justin Ng <justinng367@gmail.com>
2015-03-21 04:08:46 -0400

    stuff

--- event_loop.py
+++ event_loop.py
@@ -2,21 +2,48 @@ import time
 import clientpy2
 import broker
 import divhack
+import Queue
+
 
 def event_loop():
-    #event_queue = Queue.Queue
+    #finding original dividend rates.
+    dividend_rates = {}
+    sec = clientpy2.securities()
+    for key in sec.keys():
+        dividend_rates[key] = sec[key]['dividend_rates']
+
+    #Initializing past values
     past_orders = {}
     tickers = clientpy2.securities().keys()
     for t in tickers:
-        past_orders[t] = []
+            past_orders[t] = []
+
+    #Queue for next stock
+    next_ticker = Queue.Queue()
+    for key in dividend_rates.keys():
+        next_ticker.put(key)
+
+    active_tickers = [].append(next_ticker.get()).append(next_ticker.get()).append(next_ticker.get()).append(next_ticker.get())
+
     while True:
+        
+        update_dividends(dividend_rates);
         order_npast = push_event(past_orders, tickers)
 
         map(broker.on_broker_event, order_npast['process'])
         past_orders = order_npast['hist']
+        cycle_ticker(ticker_queue, active)
         print 'tick'
         time.sleep(0.5)
     #sleep thread
+
+def cycle_ticker(ticker_queue, active):
+    sec = clientpy2.my_securities()
+    for ticker in active:
+    #for i in range(len(active)):
+        if sec[ticker]['shares'] == 0:
+            ticker_queue.put(ticker)
+            active.index(ticker) = ticker_queue.get()
 
 #returns list to process and past orders.
 def push_event(past, tickers):
@@ -31,3 +58,10 @@ def push_event(past, tickers):
                 process.append(i)
     return {'process':process, 'hist':new_past}
 
+def update_dividends(dividend_rates):
+    securities = clientpy2.my_securities()
+    divs = securities.keys()
+    for key in divs:
+        if securities[key] != 0:
+            dividend_rates[key] = securities[key]['dividend_rates']
+    return dividend_rates
